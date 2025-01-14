2db3f5a1ed98e1d3cadaf9d7eb768488d0d780d8
navya0980 <navyakeerthi95532@gmail.com>
2025-01-14 13:14:46 +0530

    Time: 1 ms (84.73%), Space: 40.7 MB (89.86%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,18 +1,17 @@
 class Solution {
     public int reverse(int x) {
-         int num = Math.abs(x);  // Original number ka absolute value nikala
+         int num = Math.abs(x);  
         
         int rev = 0;  // Reversed number
         
         while (num != 0) {
-            int ld = num % 10;  // Last digit nikala
+            int ld = num % 10; 
             
             // Overflow check
-            if (rev > (Integer.MAX_VALUE - ld) / 10) {
-                return 0;  // Agar overflow hua, toh 0 return kardo
-            }
+            if(rev>(Integer.MAX_VALUE-ld)/10)return 0;
             
             rev = rev * 10 + ld;  // Reverse mein digit ko add kiya
+           
             num = num / 10;  // Last digit hata diya, next iteration ke liye
         }
         
