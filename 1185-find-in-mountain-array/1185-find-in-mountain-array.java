0c7084feaf80945ee60c885f842b3931e53ca5f8
navya0980 <navyakeerthi95532@gmail.com>
2025-03-29 15:09:25 +0530

    Time: 0 ms (100%), Space: 43.2 MB (80.72%) - LeetHub

--- 1185-find-in-mountain-array/1185-find-in-mountain-array.java
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.java
@@ -9,63 +9,78 @@
  
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
+        if(mountainArr.length()==1 ){
+            if(mountainArr.get(0)==target)
+             return 0;
+            else
+             return -1;
+        }
+        if(mountainArr.length()==0){
+            return -1;
+        }
         int start=0;
         int end=mountainArr.length()-1;
-       int peak= findPeak(start,end,mountainArr);
-       int ans=firstSearch(mountainArr,0,peak,target);
-       if(ans==-1)
-         ans=secondSearch(mountainArr,peak+1,mountainArr.length()-1,target);
-         
-       return ans;
+        int peak=findPeak(start,end,mountainArr);
+        if(peak!=-1 ){
+            if(mountainArr.get(peak)==target)
+            return peak;
+            
+        }
+        else
+         return binarySearch(start,end,mountainArr,target,true);
+
+      
+        int ans=binarySearch(start,peak,mountainArr,target,true);
+        if(ans==-1){
+           ans= binarySearch(peak+1,end,mountainArr,target,false);
+        }
+       
+           return ans;
+        
+        
 
        
-        
+   
     }
-    static int firstSearch(MountainArray mountainArr,int start,int end,int target){
-        int mid=-1;
-        while(start<=end){
-           mid=start+(end-start)/2;
-           if(mountainArr.get(mid)==target)
-            return mid;
-        else if(mountainArr.get(mid)>target)
-         end=mid-1;
-        else start=mid+1;
-        }
-        return -1;
-    }
-    static int secondSearch(MountainArray mountainArr,int start,int end,int target){
-        int mid=-1;
-        while(start<=end){
-           mid=start+(end-start)/2;
-           if(mountainArr.get(mid)==target)
-            return mid;
-        else if(mountainArr.get(mid)>target)
-         start=mid+1;
-        else end=mid-1;
-        }
-        return -1;
-    }
-    
-    static int findPeak(int start,int end,MountainArray mountainArr){
-       int mid=-1;
-        while(start<=end){
-           mid=start+(end-start)/2;
-            if(mountainArr.get(mid)>mountainArr.get(mid+1)){
-                if(mountainArr.get(mid)>mountainArr.get(mid-1)){
-                     return mid;
-                }
-                else{
-                     end=mid-1;
+    static int binarySearch(int start,int end,MountainArray mountainArr,int target,boolean isAsc){
+           while(start<=end)
+           {
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid)==target){
+                return mid;
+            }
+            if(isAsc==true){
+                if(mountainArr.get(mid)>target)
+                 end=mid-1;
+                 else start=mid+1;
 
-                }
-            
+            }else{
+                if(mountainArr.get(mid)>target)
+                 start=mid+1;
+                 else end=mid-1;
+
 
             }
-            else start=mid+1;
-
-        }
-        return mid;
-        
-    
+           }
+           return -1;
     }
+    
+     static int findPeak(int start,int end,MountainArray mountainArr){
+         while(start<end){
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid)>mountainArr.get(mid+1)){
+                if(mountainArr.get(mid)>mountainArr.get(mid-1)){
+                    return mid;
+                }
+                else{
+                      end=mid-1;
+                }
+                
+            }
+            else
+             start=mid+1;
+          
+        }
+        return start;
+     }
 }
