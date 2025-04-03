642580d81addaec9dc77dbce2b73bed98624790d
navya0980 <navyakeerthi95532@gmail.com>
2025-04-03 20:53:53 +0530

    Time: 0 ms (100%), Space: 56.3 MB (59.13%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -2,15 +2,20 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int s=0;
         int e=arr.length-1;
-        while(s<e){
+        while(s<=e){
             int m=s+(e-s)/2;
-            if(m<e&&arr[m]>arr[m+1]){
-               e=m;
+            if(arr[m]>arr[m+1]){
+                if(arr[m]>arr[m-1]){
+                      return m;
+                }else{
+                    e=m-1;
+                }
+             
             }else 
             {
                s=m+1;
             }
         }
-        return s;
+        return -1;
     }
 }
