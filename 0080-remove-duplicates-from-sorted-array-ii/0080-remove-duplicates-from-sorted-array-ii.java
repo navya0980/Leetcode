d5493d60cdb558e177a94a6754e1dc42281afa17
navya0980 <navyakeerthi95532@gmail.com>
2025-04-24 15:30:12 +0530

    Time: 0 ms (100%), Space: 46.8 MB (45.29%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,29 +1,44 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i=0;
-        int j=1;
-        int k=1;
-        int l=1;
-        while(i<nums.length&&j<nums.length){
-            if(nums[i]==nums[j]&&k%2!=0){
-                  int temp=nums[i+1];
-                nums[i+1]=nums[j];
-                nums[j]=temp;
-               i++;
-                k++;
-                l++;
+        // int i=0;
+        // int j=1;
+        // int k=1;
+        // int l=1;
+        // while(i<nums.length&&j<nums.length){
+        //     if(nums[i]==nums[j]&&k%2!=0){
+        //           int temp=nums[i+1];
+        //         nums[i+1]=nums[j];
+        //         nums[j]=temp;
+        //        i++;
+        //         k++;
+        //         l++;
 
-            }else if(nums[i]!=nums[j]){
-                int temp=nums[i+1];
-                nums[i+1]=nums[j];
-                nums[j]=temp;
-                i++;
+        //     }else if(nums[i]!=nums[j]){
+        //         int temp=nums[i+1];
+        //         nums[i+1]=nums[j];
+        //         nums[j]=temp;
+        //         i++;
                
-                k=1;
-                l++;
-            }
-            j++;
+        //         k=1;
+        //         l++;
+        //     }
+        //     j++;
+        // }
+        // return l;
+
+
+          int n = nums.length;
+        if(n<=2){
+            return n;
         }
-        return l;
+
+        int j = 2;
+        for(int i=2; i<n; i++){
+            if(nums[i] != nums[j-2]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
     }
 }
