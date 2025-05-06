1b1384b233a29273c97c107e6a354442118e4b99
navya0980 <navyakeerthi95532@gmail.com>
2025-05-06 18:22:07 +0530

    Time: 0 ms (100%), Space: 46.6 MB (63.37%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,44 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // int i=0;
-        // int j=1;
-        // int k=1;
-        // int l=1;
-        // while(i<nums.length&&j<nums.length){
-        //     if(nums[i]==nums[j]&&k%2!=0){
-        //           int temp=nums[i+1];
-        //         nums[i+1]=nums[j];
-        //         nums[j]=temp;
-        //        i++;
-        //         k++;
-        //         l++;
-
-        //     }else if(nums[i]!=nums[j]){
-        //         int temp=nums[i+1];
-        //         nums[i+1]=nums[j];
-        //         nums[j]=temp;
-        //         i++;
-               
-        //         k=1;
-        //         l++;
-        //     }
-        //     j++;
-        // }
-        // return l;
-
-
-          int n = nums.length;
-        if(n<=2){
-            return n;
-        }
-
-        int j = 2;
-        for(int i=2; i<n; i++){
-            if(nums[i] != nums[j-2]){
-                nums[j] = nums[i];
-                j++;
+        int j=2;
+        int i=2;
+        while(i<nums.length){
+            if(nums[i]!=nums[j-2]){
+               int temp=nums[j];
+               nums[j]=nums[i];
+               nums[i]=temp;
+               j++;
             }
+            i++;
         }
         return j;
+
+        
     }
 }
