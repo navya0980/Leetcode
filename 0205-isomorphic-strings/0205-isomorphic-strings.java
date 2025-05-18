2333618b9c7dbb7c33588d47c6d59975a1525a8d
navya0980 <navyakeerthi95532@gmail.com>
2025-05-19 00:32:28 +0530

    Time: 6 ms (92.64%), Space: 42.3 MB (84.92%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -7,11 +7,13 @@ class Solution {
       if(s.length()!=t.length())
       return false;
       for(int i=0;i<s.length();i++){
-        if(map1[s.charAt(i)]!=map2[t.charAt(i)]){
+        char ch1=s.charAt(i);
+        char ch2=t.charAt(i);
+        if(map1[ch1]!=map2[ch2]){
             return false;
         }
-        map1[s.charAt(i)]=i+1;
-        map2[t.charAt(i)]=i+1;
+        map1[ch1]=i+1;
+        map2[ch2]=i+1;
       }
            return true;
     }
