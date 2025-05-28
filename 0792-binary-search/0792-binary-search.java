bd168a392c51354e71f0a51196930f97b7d9914a
navya0980 <navyakeerthi95532@gmail.com>
2025-05-28 18:09:51 +0530

    Time: 0 ms (100%), Space: 45.5 MB (95.16%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int index=-1;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                end=mid-1;
+
+            }else{
+                start=mid+1;
+            }
+        }
+        return index;
+    }
+}
