a61ac6e7e6fa46eb0d17263a02586d3fca9485f6
navya0980 <navyakeerthi95532@gmail.com>
2025-05-29 23:46:54 +0530

    Time: 0 ms (100%), Space: 42.2 MB (53.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,55 +1,43 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==1){
-            if(nums[0]==target)
-              return 0;
-            else
-             return -1;
-             
-        }
-
-        
         int start=0;
         int end=nums.length-1;
-        if(nums[start]<nums[end])
-          return binarySearch(nums,start,end,target);
-        int pivot=findPivot(nums,start,end);
-        if(pivot!=-1){
-            if(nums[pivot]==target)
-              return pivot;
-            else if(target>=nums[0]){
-              return binarySearch(nums,0,pivot,target);
+        int peak=findPeak(nums,start,end);
+        if(peak==-1){
+          return  bs(nums,start,end,target);
         }
-             
+        if(target>=nums[0]){
+            return bs(nums,start,peak,target);
+        }else{
+             return bs(nums,peak+1,end,target);
         }
-      
-        
-       
-            return binarySearch(nums,pivot+1,nums.length-1,target);
-      
-
-        
     }
-    static int binarySearch(int[] nums,int start,int end,int target){
+    public static int findPeak(int[] nums,int start,int end){
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]==target)
-            return mid;
-            else if(nums[mid]>target)
-             end=mid-1;
-            else start=mid+1;
-
+            if(nums[mid]>=nums[0]){
+                if(mid<nums.length-1&&nums[mid]>nums[mid+1]){
+                      return mid;
+                }else{
+                  start=mid+1;
+                }
+                
+            }else {
+                end=mid-1;
+            }
         }
         return -1;
     }
-    static int findPivot(int[] nums,int start,int end){
+    public static int bs(int[] nums,int start,int end,int target){
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]>nums[mid+1])
-             return mid;
-            else if(nums[mid]>nums[nums.length-1])
-              start=mid+1;
-            else end=mid-1;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
         }
         return -1;
     }
