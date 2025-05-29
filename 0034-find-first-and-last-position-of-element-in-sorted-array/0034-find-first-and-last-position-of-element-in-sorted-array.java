83d70866217ebc4152783ec8a43d94524cd7aa25
navya0980 <navyakeerthi95532@gmail.com>
2025-05-29 23:12:08 +0530

    Time: 0 ms (100%), Space: 46.1 MB (48.47%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int start1=0;
-        int end1=nums.length-1;
-        int ans[]={-1,-1};
-        while(start1<=end1){
-            int mid=start1+(end1-start1)/2;
-            if(nums[mid]>target)
-             end1=mid-1;
-            else if(nums[mid]<target)
-            start1=mid+1;
-            else if(nums[mid]==target)
-             {
-                ans[0]=mid;
-             end1=mid-1;
-             }
-
-        }
-         int start2=0;
-        int end2=nums.length-1;
-       
-        while(start2<=end2){
-            int mid=start2+(end2-start2)/2;
-            if(nums[mid]>target)
-             end2=mid-1;
-            else if(nums[mid]<target)
-            start2=mid+1;
-            else if(nums[mid]==target)
-            {
-ans[1]=mid;
-            start2=mid+1;
+        int start=0;
+        int end=nums.length-1;
+        int[] arr={-1,-1};
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                arr[0]=mid;
+                end=mid-1;
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
-             
-
         }
-        return ans;
-
-        
+        start=0;
+        end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                arr[1]=mid;
+                start=mid+1;
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return arr;
     }
 }
