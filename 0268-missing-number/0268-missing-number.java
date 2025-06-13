a74fe8e8e5bc9595a4365c03140fcbdb3ab6d0a8
navya0980 <navyakeerthi95532@gmail.com>
2025-06-13 20:23:36 +0530

    Time: 2 ms (27.42%), Space: 45.7 MB (13.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,18 +2,15 @@ class Solution {
     public int missingNumber(int[] nums) {
         int max=Arrays.stream(nums).max().getAsInt();
         
-        for(int i=0;i<=max;i++){
-            int count=0;
-            for(int n:nums){
-                if(i==n){
-                    count=1;
-                    break;
-                }
-            }
-            if(count==0){
-                return i;
-            }
+       int[] hash=new int[max+1];
+       for(int n:nums){
+        hash[n]++;
+       }
+       for(int i=0;i<hash.length;i++){
+        if(hash[i]==0){
+            return i;
         }
-        return max+1;
+       }
+       return max+1;
     }
 }
