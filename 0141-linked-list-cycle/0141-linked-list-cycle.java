e866a400bfb6ecc500fcc20f816d4e232fd452ca
navya0980 <navyakeerthi95532@gmail.com>
2025-06-22 17:16:52 +0530

    Time: 5 ms (8.18%), Space: 45 MB (6.93%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,23 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode temp = head;
-        
-        // Create a map to keep track
-        // of encountered nodes
-        Map<ListNode, Integer> nodeMap = new HashMap<>();
-
-        // Step 2: Traverse the linked list
-        while (temp != null) {
-            // If the node is already in
-            // the map, there is a loop
-            if (nodeMap.containsKey(temp)) {
-                return true;
-            }
-            // Store the current node in the map
-            nodeMap.put(temp, 1);
-            // Move to the next node
-            temp = temp.next;
+        ListNode temp=head;
+        Map<ListNode,Integer>nodeMap=new HashMap<>();
+        while(temp!=null){
+            if(nodeMap.containsKey(temp))
+              return true;
+            
+            nodeMap.put(temp,1);
+            temp=temp.next;
         }
         return false;
     }
