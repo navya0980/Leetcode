11de3f265f2ffa10a15c5bf090b2cc402d59369e
navya0980 <navyakeerthi95532@gmail.com>
2025-06-25 19:07:17 +0530

    Time: 3 ms (99.7%), Space: 68.4 MB (51.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,46 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-     if(head==null||head.next==null){
+        ListNode slow = head, fast = head, prev, temp;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+        while (slow != null) {
+            temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        fast = head;
+        slow = prev;
+        while (slow != null) {
+            if (fast.val != slow.val) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
         return true;
-     }   
-     ListNode slow=head;
-     ListNode fast=head;
-     while(fast.next!=null&&fast.next.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-     }
-     ListNode newHead=reverse(slow.next);
-     ListNode first=head;
-     ListNode second=newHead;
-     while(second!=null){
-        if(first.val!=second.val){
-            reverse(newHead);
-            return false;
-        }
-        first=first.next;
-        second=second.next;
-     }
-     reverse(newHead);
-     return true;
-    }
-    public static ListNode reverse(ListNode head){
-        if(head==null||head.next==null){
-            return  head;
-        }
-        ListNode newHead=reverse(head.next);
-        ListNode front=head.next;
-        front.next=head;
-        head.next=null;
-        return newHead;
     }
 }
