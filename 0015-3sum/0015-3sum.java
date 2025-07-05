9f53e1d8856e6ca80f144e989dd85b583e873bbf
navya0980 <navyakeerthi95532@gmail.com>
2025-07-05 19:32:28 +0530

    Time: 1856 ms (5.01%), Space: 53.3 MB (10.3%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,21 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
-         int n=arr.length;
-        for (int i = 0; i < n; i++) {
-            //remove duplicates:
-            if (i != 0 && arr[i] == arr[i - 1]) continue;
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        HashSet<List<Integer>>st=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            HashSet<Integer>set=new HashSet<>();
+            for(int j=i+1;j<nums.length;j++){
+                int num=-(nums[j]+nums[i]);
+                if(set.contains(num)){
+                    List<Integer>inner=new ArrayList<>(Arrays.asList(num,nums[i],nums[j]));
+                    inner.sort(null);
+                   st.add(inner);
 
-            //moving 2 pointers:
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    //skip the duplicates:
-                    while (j < k && arr[j] == arr[j - 1]) j++;
-                    while (j < k && arr[k] == arr[k + 1]) k--;
                 }
+                set.add(nums[j]);
             }
         }
-
-        return ans;
+        List<List<Integer>>list=new ArrayList<>(st);
+        return list;
     }
 }
