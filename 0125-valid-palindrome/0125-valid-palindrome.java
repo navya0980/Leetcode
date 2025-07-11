5bc04aeb0ea884f8d7ccbf257568017fedeb9937
navya0980 <navyakeerthi95532@gmail.com>
2025-07-11 09:38:47 +0530

    Time: 2 ms (98.47%), Space: 43.1 MB (69.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-      
+      s=s.toLowerCase();
       int i=0;
       int j=s.length()-1;
       while(i<j){
          if(!isalnum(s.charAt(i))){i++;continue;}
          if(!isalnum(s.charAt(j))){j--;continue;}
-         if(toLower(s.charAt(i))!=toLower(s.charAt(j))){
+         if(s.charAt(i)!=s.charAt(j)){
             return false;
          }else{
             i++;
@@ -17,14 +17,8 @@ class Solution {
        return true; 
     }
     public static boolean isalnum(char ch){
-        if((ch>='a'&&ch<='z')||(ch<='9'&&ch>='0')||(ch>='A'&&ch<='Z')) return true;
+        if((ch>='a'&&ch<='z')||(ch<='9'&&ch>='0')) return true;
         else return false;
     }
-    public static char toLower(char ch){
-       Character s=ch;
-        String p=s.toString().toLowerCase();
-        return p.charAt(0);
-        
-
-    }
+   
 }
