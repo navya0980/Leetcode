cd613473bcfbad24b3e76e326535daacf7faf3b9
navya0980 <navyakeerthi95532@gmail.com>
2025-07-16 22:18:20 +0530

    Time: 5 ms (97.87%), Space: 42.7 MB (36.53%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
-        
-        int[] mapST = new int[256];
-        int[] mapTS = new int[256];
-        Arrays.fill(mapST, -1);
-        Arrays.fill(mapTS, -1);
-        
-        for (int i = 0; i < s.length(); i++) {
-            char chS = s.charAt(i);
-            char chT = t.charAt(i);
-            
-            if (mapST[chS] == -1 && mapTS[chT] == -1) {
-                mapST[chS] = chT;
-                mapTS[chT] = chS;
-            } else {
-                if (mapST[chS] != chT || mapTS[chT] != chS) {
-                    return false;
-                }
-            }
+        if(s.length()!=t.length())return false;
+       int[] hash1=new int[256];
+       int[] hash2=new int[256];
+     int index=1;
+       for(int i=0;i<s.length();i++){
+        char ch1=s.charAt(i);
+        char ch2=t.charAt(i);
+        if(hash1[ch1]!=hash2[ch2]){
+            return false;
         }
-        return true;
+        hash1[ch1]=index;
+        hash2[ch2]=index;
+        index++;
+
+
+       }
+       return true;
+
     }
 }
