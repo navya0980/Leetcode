acda9e2e1eb3a6440bee8e8f5b8bce9efcadd8c8
navya0980 <navyakeerthi95532@gmail.com>
2025-07-20 17:39:24 +0530

    Time: 25 ms (5.59%), Space: 43.5 MB (5.19%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int myAtoi(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+         int i=0;
+         int mul=1;
+         int ans=0;
+         boolean minus=false;
+         while(i<s.length()&&s.charAt(i)==' '){
+
+            i++;
+         }
+         if(i<s.length()&&s.charAt(i)=='-'){
+          minus=true;   
+            i++;
+         }else if(i<s.length()&&s.charAt(i)=='+')i++;
+         while(i<s.length()&&s.charAt(i)=='0'){
+            i++;
+         }
+         while(i<s.length()&&s.charAt(i)>='0'&&s.charAt(i)<='9'){
+           Character ch=s.charAt(i);
+           int n=ch-'0';
+           if(ans>(Integer.MAX_VALUE-n)/10){
+            if(minus==false){
+              return Integer.MAX_VALUE;  
+            }else{
+                return Integer.MIN_VALUE;
+            }
+                
+           }
+           
+            ans=ans*10+n;
+            System.out.println(n);
+            System.out.println(ans);
+
+            
+            i++;
+         }
+         if(minus==true){
+            return ans*-1;
+         }
+         return ans;
+    }
+}
