0698c9d22e92ac0d5ec19225e60804580facb4f7
navya0980 <navyakeerthi95532@gmail.com>
2025-07-27 13:48:38 +0530

    Time: 542 ms (17.13%), Space: 42.7 MB (55.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,35 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
-       
-        int maxEnd=0;
-        int maxStart=0;
-
-        for(int i=0;i<s.length();i++){
-            //odd length strings
-            int left=i,right=i;
-            while(left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)){
-               
-                left--;
-                right++;
+      int maxStart=0;
+      int maxEnd=0;
+      char ch[]=s.toCharArray();
+      for(int i=0;i<ch.length;i++){
+        for(int j=i;j<ch.length;j++){
+            if(checkPalindrome(ch,i,j)){
+                if((j-i)>(maxEnd-maxStart)){
+                    maxStart=i;
+                    maxEnd=j;
+                }
             }
-            if(right-left>maxEnd-maxStart){
-                maxStart=left+1;
-                maxEnd=right;
-            }
-            left=i;
-            right=i+1;
-            while(left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)){
-                
-                left--;
-                right++;
-            }
-            if(right-left>maxEnd-maxStart){
-                maxStart=left+1;
-                maxEnd=right;
-            }
-
         }
-        return s.substring(maxStart,maxEnd);
+      } 
+      return s.substring(maxStart,maxEnd+1);
     }
-
+    public static boolean checkPalindrome(char[] arr,int start,int end){
+        while(start<=end){
+            if(arr[start]!=arr[end])
+             return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
