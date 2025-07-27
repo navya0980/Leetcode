a254420e524ce483c084ca947af5df6ac196f624
navya0980 <navyakeerthi95532@gmail.com>
2025-07-27 14:25:46 +0530

    Time: 1235 ms (5.01%), Space: 45.3 MB (9.3%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -1,36 +1,18 @@
 class Solution {
     public int beautySum(String s) {
-        char[] chrs = s.toCharArray();
-        int res = 0;
-        for (int i = 0; i < chrs.length; i++) {
-            res += getSum(chrs, i);
+      int n=s.length();
+      int sum=0;
+       for(int i=0;i<n;i++){
+        Map<Character,Integer>map=new HashMap<>();
+        for(int j=i;j<n;j++){
+            char c=s.charAt(j);
+            int value=map.getOrDefault(c,0);
+            map.put(c,++value);
+            Integer max=Collections.max(map.values());
+             Integer min=Collections.min(map.values());
+            sum+=max-min;
         }
-        return res;
-    }
-
-    public int getSum(char[] chrs, int i) {
-        int[] counts = new int[26];
-        counts[chrs[i] - 'a']++;
-        int max = 1;
-        int min = 1;
-        int res = 0;
-        for (int j = i + 1; j < chrs.length; j++) {
-            int count = counts[chrs[j] - 'a']++;
-            if (count == max)
-                max++;
-            if (count == 0) {
-                min = 1;
-            } else if (count == min) {
-                min++;
-                for (int k = 0; k < 26; k++) {
-                    if (counts[k] != 0 && counts[k] < min) {
-                        min--;
-                        break;
-                    }
-                }
-            }
-            res += max - min;
-        }
-        return res;
+       } 
+       return sum;
     }
 }
