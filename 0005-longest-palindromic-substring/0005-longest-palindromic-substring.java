2b50f80b44d87cd30eb3a0303f541556487c7155
navya0980 <navyakeerthi95532@gmail.com>
2025-07-27 13:59:30 +0530

    Time: 61 ms (39.06%), Space: 42.5 MB (63.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,23 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-      int maxStart=0;
-      int maxEnd=0;
+      int maxStart=0,maxEnd=0;
+     
       char ch[]=s.toCharArray();
-      for(int i=0;i<ch.length;i++){
-        for(int j=i;j<ch.length;j++){
-            if(checkPalindrome(ch,i,j)){
-                if((j-i)>(maxEnd-maxStart)){
-                    maxStart=i;
-                    maxEnd=j;
-                }
+       int i=0;
+      int j=ch.length-1;
+      while(i<=j){
+        while(j>=0&&ch[i]!=ch[j]){
+            j--;
+        }
+        if(j>=0&&checkPalindrome(ch,i,j)){
+            if((j-i)>(maxEnd-maxStart)){
+                maxStart=i;
+                maxEnd=j;
+            }else{
+                i++;
+                j=ch.length-1;
             }
+        }else{
+            j--;
         }
       } 
-      return s.substring(maxStart,maxEnd+1);
+      return s.substring(maxStart,maxEnd+1); 
     }
-    public static boolean checkPalindrome(char[] arr,int start,int end){
+    public static boolean checkPalindrome(char[] ch,int start,int end){
         while(start<=end){
-            if(arr[start]!=arr[end])
+            if(ch[start]!=ch[end])
              return false;
             start++;
             end--;
