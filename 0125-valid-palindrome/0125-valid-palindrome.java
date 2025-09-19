b2a95ccfc0ec6eb3e02a1733b43bae842b485afd
navya0980 <navyakeerthi95532@gmail.com>
2025-09-19 18:20:14 +0530

    Time: 7 ms (45.15%), Space: 50 MB (5.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,24 +3,30 @@ class Solution {
         StringBuilder str=new StringBuilder(s.toLowerCase());
         int left=0;
         int right=str.length()-1;
-        while(left<right){
-            char ch1=str.charAt(left);
-            char ch2=str.charAt(right);
-            if((ch1>='a'&&ch1<='z')||(ch1>='0'&&ch1<='9')){
-                if((ch2>='a'&&ch2<='z')||(ch2>='0'&&ch2<='9')){
-                    if(ch1!=ch2){
-                        return false;
-                    }
-                    left++;
-                    right--;
-                }else{
-                    right--;
-                }
-
-            }else{
-                left++;
-            }
+        return check(str,left,right);
+    }
+    public static boolean check(StringBuilder str,int left,int right){
+       
+        if(left>=right){
+            return true;
         }
-        return true;
+         char ch1=str.charAt(left);
+        char ch2=str.charAt(right);
+        if((ch1>='a'&&ch1<='z')||(ch1>='0'&&ch1<='9')){
+            if((ch2>='a'&&ch2<='z')||(ch2>='0'&&ch2<='9')){
+                if(ch1!=ch2)
+                  return false;
+                else 
+                  return check(str,++left,--right);
+            }else{
+                return check(str,left,--right);
+            }
+
+        }else{
+            return check(str,++left,right);
+        }
+        
+
+        
     }
 }
