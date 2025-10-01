3b3e896ff137bd8b802290ec03e6cfe6cfd46bd2
navya0980 <navyakeerthi95532@gmail.com>
2025-10-01 22:14:27 +0530

    Time: 3 ms (100%), Space: 76.6 MB (88.5%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         //BRUTEFORCE
-        int n=nums.length;
-       int[] pos=new int[n/2];
-       int[] neg=new int[n/2];
-       int j=0,k=0;
-       for(int i=0;i<n;i++){
-        if(nums[i]<0){
-            neg[k]=nums[i];
-            k++;
-        }else{
-            pos[j]=nums[i];
-            j++;
+        int[] ans=new int[nums.length];
+        int pos=0,neg=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                ans[neg]=nums[i];
+                neg+=2;
+            }else{
+                ans[pos]=nums[i];
+                pos+=2;
+            }
         }
-       } 
-       for(int i=0;i<n/2;i++){
-        nums[i*2]=pos[i];
-        nums[i*2+1]=neg[i];
-       }
-       return nums;
+        return ans;
     }
 }
