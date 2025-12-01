5971e918fa0c23fb910a396287f8f18f93de47e9
navya0980 <navyakeerthi95532@gmail.com>
2025-12-01 13:58:12 +0530

    Time: 3 ms (2.36%), Space: 43.6 MB (33.99%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,32 +1,56 @@
 class Solution {
-    public int search(int[] arr, int k) {
-        int low = 0, high = arr.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            // if mid points to the target
-            if (arr[mid] == k)
-                return mid;
-
-            // if left part is sorted
-            if (arr[low]  <= arr[mid]) {
-                if (arr[low] <= k && k <= arr[mid]) {
-                    // element exists
-                    high = mid - 1;
-                } else {
-                    // element does not exist
-                    low = mid + 1;
-                }
-            } else { // if right part is sorted
-                if (arr[mid] <= k && k <= arr[high]) {
-                    // element exists
-                    low = mid + 1;
-                } else {
-                    // element does not exist
-                    high = mid - 1;
-                }
-            }
+    public int search(int[] nums, int target) {
+      if(nums.length==0){
+        return -1;
+      }
+      if(nums.length==1){
+        if(nums[0]==target){
+            return 0;
+        }else{
+            return -1;
+        }
+      }
+      int peak=findPeak(nums);
+      System.out.println(peak);
+      if(target==nums[peak]){
+        return peak;
+      }
+      if(target>=nums[0]&&target<nums[peak]){
+       return binarySearch(nums,0,peak,target);
+      }
+       return binarySearch(nums,peak+1,nums.length-1,target);
+      
+      
+      
+    }
+    public static int binarySearch(int[] nums,int start,int end,int target){
+        while(start<=end){
+         int mid=start+(end-start)/2;
+         if(nums[mid]==target){
+            return mid;
+         }else if(nums[mid]<target){
+            start=mid+1;
+         }else{
+            end=mid-1;
+         }
         }
         return -1;
+    }
+    public static int findPeak(int[] nums){
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid<nums.length-1&&nums[mid]>nums[mid+1]){
+                return mid;
+            }else if(mid>0&&nums[mid]<nums[mid-1]){
+                return mid-1;
+            }else if(nums[mid]<=nums[0]){
+                end=mid-1;
+            }else if(nums[mid]>=nums[0]){
+                start=mid+1;
+            }
+        }
+        return nums.length-1;
     }
 }
