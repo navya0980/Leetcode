d0832fa8c1b52c64c01c24e31897667c68c7f647
navya0980 <navyakeerthi95532@gmail.com>
2025-12-01 11:48:44 +0530

    Time: 0 ms (100%), Space: 48 MB (36.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,47 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int start=0;
-        int end=nums.length-1;
-        int arr[]={-1,-1};
+        int start=0,end=nums.length-1;
+        int[] arr={-1,-1};
         while(start<=end){
             int mid=start+(end-start)/2;
             if(nums[mid]==target){
-                arr[0]=mid;
-                end=mid-1;
+                arr[0]=mid;arr[1]=mid;
+                binarySearch1(nums,start,mid-1,target,arr);
+                binarySearch2(nums,mid+1,end,target,arr);
+                return arr;
 
-
-            }else if(nums[mid]>target){
-                end=mid-1;
+            }else if(nums[mid]<target){
+                start=mid+1;
             }else{
-                start=mid+1;
-            }
-        }
-        start=0;
-       end=nums.length-1;
-       
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                arr[1]=mid;
-                start=mid+1;
-
-
-            }else if(nums[mid]>target){
                 end=mid-1;
-            }else{
-                start=mid+1;
             }
         }
         return arr;
+    }
+    public static void binarySearch1(int[] nums,int start,int end,int target,int[] arr){
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+               arr[0]=mid;end=mid-1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+    }
+     public static void binarySearch2(int[] nums,int start,int end,int target,int[] arr){
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+               arr[1]=mid;start=mid+1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
     }
 }
