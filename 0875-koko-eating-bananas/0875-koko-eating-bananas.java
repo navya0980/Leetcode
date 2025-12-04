bfe96331e38ea7dad162aa6ae4a5945d4f8044cd
navya0980 <navyakeerthi95532@gmail.com>
2025-12-04 10:06:09 +0530

    Time: 14 ms (53.62%), Space: 47.8 MB (34.21%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,29 +1,30 @@
 class Solution {
-
-    private int check(int[] piles, int h, int mid) {
-        int ans = 0;
-        for (int pile : piles) {
-            ans += pile / mid;
-            if (pile % mid != 0) {
-                ans++;
-            }
-        }
-        return ans <= h ? 1 : 0;
-    }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int n = piles.length;
-        int low = 1;
-        int high = Arrays.stream(piles).max().getAsInt();
-
-        while (low < high) {
-            int mid = (low + high) >> 1;
-            if (check(piles, h, mid) == 1) {
-                high = mid;
-            } else {
-                low = mid + 1;
+        int low=1;
+        int high=Arrays.stream(piles).max().getAsInt();
+        
+       int min=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isMin(mid,h,piles)){
+                min=Math.min(min,mid);
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return low;
+    }
+    public  static boolean isMin(int min,int hours,int[] arr){
+        long h=0;
+        for(int i=0;i<arr.length;i++){
+           h+=arr[i]/min;
+           if(arr[i]%min!=0){
+            h++;
+           }
+         
+        }
+      return h<=hours;
+
     }
 }
