6e042d898f673bcab62551afc24954135dcf8339
navya0980 <navyakeerthi95532@gmail.com>
2025-12-15 12:00:55 +0530

    Time: 1 ms (99.94%), Space: 43.8 MB (37.81%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -5,55 +5,29 @@ class Solution {
          if(s.length()==0){
             return 0;
         }
-      int ans=0;
+     
       int isNeg=0;
      
         if(s.charAt(0)=='+'){
              
-             for(int i=1;i<s.length();i++){
-               int ch=s.charAt(i)-'0';
-              
-                if(ch>=0&&ch<=9){
-                     if(ans>(Integer.MAX_VALUE-ch)/10){
-                if(isNeg==1){
-                    return Integer.MIN_VALUE;
-                }else{
-                    return Integer.MAX_VALUE;
-                }
-               }
-                  ans=ans*10+ch;
-
-                }else{
-                    break;
-                }
-            }
+           return  findNum(s,isNeg,1);
         }else if(s.charAt(0)=='-'){
         
           isNeg=1;
           
-             for(int i=1;i<s.length();i++){
+             return  findNum(s,isNeg,1);
+        }
+
+        return  findNum(s,isNeg,0);
+        
+       
+    
+    }
+    public static int findNum(String s,int isNeg,int index){
+         int ans=0;
+          for(int i=index;i<s.length();i++){
                int ch=s.charAt(i)-'0';
               
-               
-                if(ch>=0&&ch<=9){
-                     if(ans>(Integer.MAX_VALUE-ch)/10){
-                if(isNeg==1){
-                    return Integer.MIN_VALUE;
-                }else{
-                    return Integer.MAX_VALUE;
-                }
-               }
-                  ans=ans*10+ch;
-                 
-
-                }else{
-                    break;
-                }
-            }
-        }else{
-            for(int i=0;i<s.length();i++){
-                int ch=s.charAt(i)-'0';
-               
                 if(ch>=0&&ch<=9){
                      if(ans>(Integer.MAX_VALUE-ch)/10){
                 if(isNeg==1){
@@ -65,13 +39,13 @@ class Solution {
                   ans=ans*10+ch;
 
                 }else{
-                    break;
+                   break;  
                 }
             }
-        }
-      if(isNeg==1){
-        return ans*-1;
-      }
-      return ans;
+            if(isNeg==1){
+                         return ans*-1;
+                       }
+                    return ans;
+
     }
 }
