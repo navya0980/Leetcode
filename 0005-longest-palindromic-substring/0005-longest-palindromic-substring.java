83e852a416ceaf32420e5b91b00abfc7f14bab2a
navya0980 <navyakeerthi95532@gmail.com>
2025-12-15 13:45:30 +0530

    Time: 18 ms (50.95%), Space: 43.3 MB (80.68%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,35 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
-      int maxStart=0,maxEnd=0;
-     
-     char ch[]=s.toCharArray();
-     int n=ch.length;
-     for(int i=0;i<n;i++){
-        //for odd length strings
-        int start=i,end=i;
-        while(start>=0&&end<n&&ch[start]==ch[end]){
-            start--;
-            end++;
+        int max=0;
+        int maxStart=-1,maxEnd=-1;
+        for(int i=0;i<s.length();i++){
+                 int start=i,end=i;
+                 while(start>=0&&end<s.length()&&s.charAt(start)==s.charAt(end)){
+                    int len=end-start+1;
+                    if(len>max){
+                        max=len;
+                        maxStart=start;
+                        maxEnd=end;
+                    }
+                    start--;end++;
+                 }
+                 start=i;end=i+1;
+                 while(start>=0&&end<s.length()&&s.charAt(start)==s.charAt(end)){
+                    int len=end-start+1;
+                    if(len>max){
+                        max=len;
+                        maxStart=start;
+                        maxEnd=end;
+                    }
+                    start--;end++;
+                 }
         }
-        if(end-start>maxEnd-maxStart){
-            maxStart=start+1;
-            maxEnd=end;
-        }
-        //for even length strings
-        start=i;
-        end=i+1;
-        while(start>=0&&end<n&&ch[start]==ch[end]){
-            start--;
-            end++;
-        }
-        if(end-start>maxEnd-maxStart){
-            maxStart=start+1;
-            maxEnd=end;
-        }
-
-
-     }
-      return s.substring(maxStart,maxEnd); 
+        return s.substring(maxStart,maxEnd+1);
     }
-    
 }
