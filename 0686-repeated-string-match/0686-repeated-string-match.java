f15657f31319ada02263f02b8b806f0cbef2cbcb
navya0980 <navyakeerthi95532@gmail.com>
2025-12-22 21:37:02 +0530

    Time: 2446 ms (5.11%), Space: 44.5 MB (30.4%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,15 +1,15 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         int count=1;
-        String c=a;
+        StringBuilder c=new StringBuilder(a);
         while(c.indexOf(b)==-1){
            if(c.length()>10000){
             return -1;
            }
-            c+=a;
+           c.append(a);
            
             count++;
-            System.out.println(a);
+           
         }
         return count;
     }
