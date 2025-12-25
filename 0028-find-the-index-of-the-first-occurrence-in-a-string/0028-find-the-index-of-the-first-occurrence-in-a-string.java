572ae761630d5033d8c3da73217624569d086018
navya0980 <navyakeerthi95532@gmail.com>
2025-12-25 12:27:46 +0530

    Time: 2 ms (8.42%), Space: 42.9 MB (56.04%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,35 @@
 class Solution {
+    private static final int prime=101;
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        if(haystack.length()<needle.length()){
+            return -1;
+        }
+        int patternLen=needle.length();
+       double haystackHash=calculateHash(haystack.substring(0,patternLen));
+       double needleHash=calculateHash(needle);
+       for(int i=0;i<=haystack.length()-patternLen;i++){
+        if(haystackHash==needleHash){
+            if(haystack.substring(i,i+patternLen).equals(needle)){
+                return i;
+            }
+        }
+        if(i<haystack.length()-patternLen){
+            haystackHash=updateHash(haystackHash,haystack.charAt(i),haystack.charAt(i+patternLen),patternLen);
+        }
+       }
+       return -1;
+
+    }
+    public static double calculateHash(String str){
+        double hash=0;
+        for(int i=0;i<str.length();i++){
+            hash+=str.charAt(i)*Math.pow(prime,i);
+        }
+        return hash;
+    }
+    public static double updateHash(double prevHash,char oldChar,char newChar,int patternLen){
+        double newHash=(prevHash-oldChar)/prime;
+        newHash=newHash+newChar*Math.pow(prime,patternLen-1);
+        return newHash;
     }
 }
