43bf4e14368395f7a424113306d36843462fba0a
navya0980 <navyakeerthi95532@gmail.com>
2025-12-25 12:32:55 +0530

    Time: 271 ms (20.61%), Space: 47 MB (6.11%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,16 +1,19 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int count=1;
-        StringBuilder c=new StringBuilder(a);
-        while(c.indexOf(b)==-1){
-           if(c.length()>10000){
-            return -1;
-           }
-           c.append(a);
-           
-            count++;
-           
-        }
-        return count;
+     String c=a;
+     int count=1;
+     while(c.length()<b.length()){
+        c=c+a;
+        count++;
+     } 
+     if(c.contains(b)){
+            return count;
+     }  
+     c+=a;
+     if(c.contains(b)){
+            return ++count;
+     }
+     return -1;
+
     }
 }
