a98c0b28c5e9763fc427df84fd4340afe6d822a0
navya0980 <navyakeerthi95532@gmail.com>
2026-01-07 16:48:08 +0530

    Time: 1 ms (35.91%), Space: 43.3 MB (17.56%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,35 +1,27 @@
 class Solution {
-    private static final int prime=101;
-    public int strStr(String haystack, String needle) {
-        if(haystack.length()<needle.length()){
-            return -1;
-        }
-        int patternLen=needle.length();
-       double haystackHash=calculateHash(haystack.substring(0,patternLen));
-       double needleHash=calculateHash(needle);
-       for(int i=0;i<=haystack.length()-patternLen;i++){
-        if(haystackHash==needleHash){
-            if(haystack.substring(i,i+patternLen).equals(needle)){
-                return i;
+    public int strStr(String source, String target) {
+         int m=target.length();
+       int power=1;
+       for(int i=0;i<m;i++){
+        power=(power*31);
+       }
+       int targetCode=0;
+       for(int i=0;i<m;i++){
+        targetCode=(targetCode*31+target.charAt(i));
+       }
+       int hashCode=0;
+       for(int i=0;i<source.length();i++){
+          hashCode=(hashCode*31+source.charAt(i));
+          if(i>=m){
+            hashCode=(hashCode-source.charAt(i-m)*power);
+          }
+          if(hashCode==targetCode){
+            if(source.substring(i-m+1,i+1).equals(target)){
+                return i-m+1;
             }
-        }
-        if(i<haystack.length()-patternLen){
-            haystackHash=updateHash(haystackHash,haystack.charAt(i),haystack.charAt(i+patternLen),patternLen);
-        }
+          }
        }
        return -1;
-
-    }
-    public static double calculateHash(String str){
-        double hash=0;
-        for(int i=0;i<str.length();i++){
-            hash+=str.charAt(i)*Math.pow(prime,i);
-        }
-        return hash;
-    }
-    public static double updateHash(double prevHash,char oldChar,char newChar,int patternLen){
-        double newHash=(prevHash-oldChar)/prime;
-        newHash=newHash+newChar*Math.pow(prime,patternLen-1);
-        return newHash;
+    
     }
 }
