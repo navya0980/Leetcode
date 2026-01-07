d5509891e2667976445e9c5fdd42998392ebc9ac
navya0980 <navyakeerthi95532@gmail.com>
2026-01-07 06:09:18 +0530

    Time: 272 ms (17.97%), Space: 46.9 MB (7.67%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,20 +1,23 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        if(a.equals(b)) return 1;
-     StringBuilder c=new StringBuilder(a);
-     int count=1;
-     while(c.length()<b.length()){
-        c.append(a);
-        count++;
-     } 
-     if(c.indexOf(b)!=-1){
-            return count;
-     }  
-    c.append(a);
-     if(c.indexOf(b)!=-1){
-            return count+1;
-     }
-     return -1;
+        if(a.equals(b)){
+            return 1;
+        }
+        String c=a;
+        int count=1;
+        while(c.length()<b.length()){
+            c+=a;
+            count++;
+        }
+        if(!c.contains(b)){
+           
+             c+=a;
+            count++;
+        }
+        if(!c.contains(b)){
+           return -1;
+        }
 
+         return count;
     }
 }
