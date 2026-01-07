32febf1af18e3648690fc686220042b5fe75bb8d
navya0980 <navyakeerthi95532@gmail.com>
2026-01-07 16:45:09 +0530

    Time: 12 ms (89.43%), Space: 46.9 MB (13.86%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,23 +1,42 @@
 class Solution {
+    private static int BASE=1000000;
     public int repeatedStringMatch(String a, String b) {
-        if(a.equals(b)){
-            return 1;
-        }
-        StringBuilder c=new StringBuilder(a);
-        int count=1;
-        while(c.length()<b.length()){
-            c.append(a);
-            count++;
-        }
-        if(c.indexOf(b)==-1){
-           
-            c.append(a);
-            count++;
-        }
-        if(c.indexOf(b)==-1){
-           return -1;
-        }
-
-         return count;
+       if(a.equals(b)){
+        return 1;
+       } 
+       String c=a;
+       int count=1;
+       while(c.length()<b.length()){
+        c+=a;
+        count++;
+       }
+       if(c.equals(b))return count;
+       if((rabinKarp( c, b))!=-1)return count;
+       if((rabinKarp( c+a, b))!=-1)return count+1;
+       return -1;
+    }
+    public static int rabinKarp(String source,String target){
+       int m=target.length();
+       int power=1;
+       for(int i=0;i<m;i++){
+        power=(power*31);
+       }
+       int targetCode=0;
+       for(int i=0;i<m;i++){
+        targetCode=(targetCode*31+target.charAt(i));
+       }
+       int hashCode=0;
+       for(int i=0;i<source.length();i++){
+          hashCode=(hashCode*31+source.charAt(i));
+          if(i>=m){
+            hashCode=(hashCode-source.charAt(i-m)*power);
+          }
+          if(hashCode==targetCode){
+            if(source.substring(i-m+1,i+1).equals(target)){
+                return i;
+            }
+          }
+       }
+       return -1;
     }
 }
