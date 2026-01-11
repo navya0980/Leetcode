46579a6f32f8e4c226d332a220312e90b5a68cd5
navya0980 <navyakeerthi95532@gmail.com>
2026-01-11 10:33:12 +0530

    Time: 3 ms (99.76%), Space: 94.7 MB (43.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,18 +10,32 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode temp=head;
-        StringBuilder str=new StringBuilder();
-        while(temp!=null){
-            str.append(temp.val+"");
-
-            temp=temp.next;
-
-        }
-      for(int i=0;i<str.length()/2;i++){
-        if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+      ListNode temp=head;
+      int size=0;
+      while(temp!=null){
+         size++;
+         temp=temp.next;
+      } 
+      temp=head;
+      ListNode prev=null,node=null;
+      for(int i=0;i<size/2;i++){
+         node=temp.next;
+         if(temp==head){
+            temp.next=null;
+         }else{
+            temp.next=prev;
+         }
+         prev=temp;
+         temp=node;
+      } 
+      if(size%2!=0&&node!=null){
+        node=node.next;
+      }
+      while(prev!=null&&node!=null){
+        if(prev.val!=node.val){
             return false;
         }
+        prev=prev.next;node=node.next;
       }
       return true;
     }
