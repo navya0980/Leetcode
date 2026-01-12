735d6f7276dce5c805f09c0276387133f03e9137
navya0980 <navyakeerthi95532@gmail.com>
2026-01-12 06:26:38 +0530

    Time: 0 ms (100%), Space: 42.3 MB (20.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+         int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        // Base cases
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // Fill dp array using bottom-up dynamic programming
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
