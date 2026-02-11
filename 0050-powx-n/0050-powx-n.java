b76cae66f834bf1e58e0db0a01b0d309ab70ecdf
navya0980 <navyakeerthi95532@gmail.com>
2026-02-11 09:46:31 +0530

    Time: 0 ms (100%), Space: 48 MB (56.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,28 +1,26 @@
 class Solution {
     public double myPow(double x, int n) {
+        double num=1;
         if(n==Integer.MIN_VALUE&&x!=1&&x!=-1){
             return 0;
         }
-        double num=1;
-        double ans=findPow(x,num,Math.abs(n));
+        double ans= getPow(x,num,Math.abs(n));
         if(n<0){
             return 1/ans;
         }
         return ans;
     }
-    static double findPow(double x,double num,int n){
-       if(n<=0){
-        return num;
-       }
-       if(n%2==1){
-        num=num*x;
-        n--;
-       }else{
-        x*=x;
-        n/=2;
-       }
-       
-       return findPow(x,num,n);
-        
+    static double getPow(double x,double num,int n){
+        if(n<=0){
+            return num;
+        }
+        if(n%2==1){
+            num=num*x;
+            n--;
+        }else{
+            x=x*x;
+            n/=2;
+        }
+        return getPow(x,num,n);
     }
 }
