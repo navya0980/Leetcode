3c22d3aae63f5bd82202fc8bbe785fd25e9ecf4e
navya0980 <navyakeerthi95532@gmail.com>
2026-03-22 21:29:04 +0530

    Time: 9 ms (37.33%), Space: 56.4 MB (5.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l=0,r=s.length()-1;
-        while(l<r){
-            char ch1=s.charAt(l),ch2=s.charAt(r);
-            if(!Character.isLetterOrDigit(ch1)){
-                l++;
+       
+        return func(s,0,s.length()-1);
+       
+    }
+    static boolean func(String s,int l,int r){
+        if(l>r){
+            return true;
+        }
+        char ch1=s.charAt(l),ch2=s.charAt(r);
+        if(!Character.isLetterOrDigit(ch1)){
+               return func(s,++l,r);
             }else if(!Character.isLetterOrDigit(ch2)){
-                r--;
+             return  func(s,l,--r);
             }else if(Character.toLowerCase(ch1)!=Character.toLowerCase(ch2)){
                 return false;
-            }else{
-                l++;r--;
             }
-        }
-        return true;
+              return func(s,++l,--r);
+            
     }
 }
