43cb30569e2dd96b5b3dbb15d196d06e6da6aed2
navya0980 <navyakeerthi95532@gmail.com>
2026-03-28 21:10:08 +0530

    Time: 17 ms (8.85%), Space: 45.3 MB (9.85%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,17 +1,37 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-       List<Integer>list=new ArrayList<>();
-        
-        for(int i=0;i<n1;i++){
-            int num=nums1[i];
-            for(int j=0;j<n2;j++){
-                if(nums2[j]==num&&!list.contains(num)){
-                  list.add(num);
-                }
+        List<Integer>list1=new ArrayList<>();
+        List<Integer>list2=new ArrayList<>();
+        List<Integer>list=new ArrayList<>();
+
+        int left=0,right=0;
+        while(left<nums1.length&&right<nums2.length){
+            int num1=nums1[left],num2=nums2[right];
+            if(list2.contains(num1)&&!list.contains(num1)){
+               list.add(num1);
             }
+            list1.add(num1);
+            if(list1.contains(num2)&&!list.contains(num2)){
+               list.add(num2);
+            }
+            list2.add(num2);
+            left++;right++;
+        }
+        while(left<nums1.length){
+            int num1=nums1[left];
+            if(list2.contains(num1)&&!list.contains(num1)){
+               list.add(num1);
+            }
+            left++;
+        }
+        while(right<nums2.length){
+            int num2=nums2[right];
+            if(list1.contains(num2)&&!list.contains(num2)){
+               list.add(num2);
+            }
+            left++;right++;
         }
         return list.stream().mapToInt(Integer::intValue).toArray();
+        
     }
 }
