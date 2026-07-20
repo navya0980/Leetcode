9fcc4a39343ee3a73952be0ebd592f6d59cf8ede
navya0980 <navyakeerthi95532@gmail.com>
2026-07-20 20:35:34 +0530

    Time: 4 ms (100%), Space: 45.8 MB (87.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,12 +3,12 @@ class Solution {
         if(x<0){
             return false;
         }
-        int n=x;
-        int res=0;
-        while(n>0){
-            res=res*10+(n%10);
-            n=n/10;
+        int ans=0;
+        int num=x;
+        while(num>0){
+            ans=ans*10+num%10;
+            num=num/10;
         }
-        return res==x;
+        return ans==x;
     }
 }
