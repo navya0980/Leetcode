42f95fc8276090cb3662c2f0a36771b53af7f30c
navya0980 <navyakeerthi95532@gmail.com>
2026-07-20 22:55:00 +0530

    Time: 7 ms (36.52%), Space: 47.2 MB (14.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.trim().toLowerCase();
-        
-        int st=0,e=s.length()-1;
-        while(st<e){
-            char ch1=s.charAt(st);
-            char ch2=s.charAt(e);
-            if((ch1>='a'&&ch1<='z')||(ch1>='0'&&ch1<='9')){
-                if((ch2>='a'&&ch2<='z')||(ch2>='0'&&ch2<='9')){
-                    if(ch1!=ch2)
-                     return false;
-                    else
-                     st++;e--;
-                }else{
-                    e--;
-                }
-            }else{
-                st++;
+        s = s.toLowerCase();
+
+        StringBuilder str = new StringBuilder("abcdefghijklmnopqrstuvwxyz0123456789");
+
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+
+            while (i < j && str.indexOf(String.valueOf(s.charAt(i))) == -1) {
+                i++;
             }
+
+            while (i < j && str.indexOf(String.valueOf(s.charAt(j))) == -1) {
+                j--;
+            }
+
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
         }
+
         return true;
     }
 }
