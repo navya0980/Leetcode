6c23c2a75cf4da360643069cc0633772abf5d49e
navya0980 <navyakeerthi95532@gmail.com>
2026-08-13 18:38:54 +0530

    Time: 0 ms (100%), Space: 52.9 MB (59.04%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,37 +1,26 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int start=0;
-        int end=nums.length-1;
-        int dupIndex=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-           
-            int len=mid-start+1;
-            if(mid>0 &&  nums[mid-1]==nums[mid]){
-                 dupIndex=mid-1;
-            }else if(mid<nums.length-1 && nums[mid+1]==nums[mid]){
-                dupIndex=mid+1;
+        int s=0,e=nums.length-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            
+            if(m>0&&nums[m]==nums[m-1]){
+              
+              if(m%2!=0){
+                s=m+1;
+              }else{
+                e=m-2;
+              }
+            }else if(m<e&&nums[m]==nums[m+1]){
+                if(m%2!=0){
+               e=m-1;
+              }else{
+                s=m+2;
+              }
+            }else{
+                return nums[m];
             }
-            else{
-                return nums[mid];
-            }
-
-            String isEvenOrOdd=(len%2==0)?"even":"odd";
-            if(start<=dupIndex&&dupIndex<=mid ){
-                if(isEvenOrOdd=="odd"){
-                    
-                     end=mid-2;
-                }else{
-                   start=mid+1;
-                }
-            }else {
-                if(isEvenOrOdd=="even"){
-                    end=mid-1;
-                }else{
-                    start=mid+2;
-                }
-            }
-         }
-         return nums[end];
+        }
+        return -1;
     }
 }
