a82700b28bd022197009d4e96bfb152953714a8e
navya0980 <navyakeerthi95532@gmail.com>
2026-08-13 17:05:05 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
+       
         int pivot=findPivot(nums);
-        if(pivot==-1){
-            return bs(nums,target,0,nums.length-1);
+        
+        if(target<=nums[nums.length-1]){
+            return bs(nums,target,pivot,nums.length-1);
         }
-        if(target>=nums[0]){
-            return bs(nums,target,0,pivot);
-        }
-        return bs(nums,target,pivot+1,nums.length-1);
+        return bs(nums,target,0,pivot-1);
 
     }
-    static int findPivot(int[] nums){
-        int s=0,e=nums.length-1;
-        while(s<e){
-            int m=s+(e-s+1)/2;
-            if(nums[m]>nums[0]){
-                s=m;
-            }else{
-                e=m-1;
-            }
+    static int findPivot(int[] nums) {
+    int s = 0, e = nums.length - 1;
+
+    while (s < e) {
+        int m = s + (e - s) / 2;
+
+        if (nums[m] > nums[e]) {
+            s = m + 1;
+        } else {
+            e = m;
         }
-        return e;
     }
+
+    return s;
+}
     static int bs(int[] nums,int target,int s,int e){
         while(s<=e){
             int m=s+(e-s)/2;
