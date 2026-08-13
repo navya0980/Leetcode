7550fae8a0acdf93cb9aceed8b935e894c9d09b6
navya0980 <navyakeerthi95532@gmail.com>
2026-08-13 16:38:41 +0530

    Time: 0 ms (100%), Space: 44 MB (28.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,24 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[mid]>=nums[start]){
-                if(nums[start]<=target&&target<=nums[mid]){
-                      end=mid-1;
-                }else{
-                    start=mid+1;
-                }
+        int pivot=findPivot(nums);
+        if(pivot==-1){
+            return bs(nums,target,0,nums.length-1);
+        }
+        if(target>=nums[0]){
+            return bs(nums,target,0,pivot);
+        }
+        return bs(nums,target,pivot+1,nums.length-1);
+
+    }
+    static int findPivot(int[] nums){
+        int s=0,e=nums.length-1;
+        while(s<e){
+            int m=s+(e-s+1)/2;
+            if(nums[m]>nums[0]){
+                s=m;
             }else{
-                if(nums[mid]<=target&&target<=nums[end]){
-                    start=mid+1;
-                }else{
-                    end=mid-1;
-                }
+                e=m-1;
+            }
+        }
+        return e;
+    }
+    static int bs(int[] nums,int target,int s,int e){
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(nums[m]==target){
+                return m;
+            }else if(nums[m]<target){
+                s=m+1;
+            }else{
+                e=m-1;
             }
         }
         return -1;
